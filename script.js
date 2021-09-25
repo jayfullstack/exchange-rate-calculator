38ff7874e24ebf682a36f93352832feeceea0dc7
jayfullstack <jayfullstack@yahoo.com>
2021-09-25 08:43:35 -0400

    Experimented with looping through the data obj

--- script.js
+++ script.js
@@ -6,11 +6,27 @@ const amountEl_two = document.getElementById('amount-two');
 const rateEl = document.getElementById('rate');
 const swap = document.getElementById('swap');
 
+// Generates the HTML select options for all countries
+// Useful if I wanted to use them to generate the menu options
+
+// function getRates(){
+//   fetch('https://api.exchangerate-api.com/v4/latest/USD')
+//     .then(res => res.json())
+//     .then(data => {
+//       console.log(data.rates);
+//       Object.keys(data.rates).forEach(key =>{
+//         console.log(key);
+//       });
+//     });
+// }
+// getRates();
+
 // Fetch exchange rates and update the DOM
 // Fetch won't reject on HTTP Error Status (unlike AJAX)
 // Only rejects on something preventing it from firing
 // Link to more info on Fetch Requests
 // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+
 function caclulate() {
   const currency_one = currencyEl_one.value;
   const currency_two = currencyEl_two.value;
